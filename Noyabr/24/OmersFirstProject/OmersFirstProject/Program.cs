97d480af07e6a27fef116bb3d0a1d33529ee0032
Omer Mustafa <omar201020222022@gmail.com>
2022-11-24 22:07:59 +0400

    ilk proektin yaradilmasi

---
+++ Noyabr/24/OmersFirstProject/OmersFirstProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmersFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salam xos gelmisiniz!");
+            Console.WriteLine("Adinizi daxil edin:");
+            Console.ReadLine();
+        }
+    }
+}
