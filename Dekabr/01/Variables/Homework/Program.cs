8d76420251cd2c47351d4cef46e63206ef6323d8
Orkhan Farajov <orkhankhf@gmail.com>
2022-12-01 10:40:13 +0400

    Variables lesson

---
+++ Dekabr/01/Variables/Homework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Homework 1:
+             iki dənə rəqəm olacaq biri 10 digəri 20. Bu 10 və 20 rəqəmlərini a və b adında iki fərqli dəyişəndə saxla.
+             Sonra isə, üçüncü bir dəyişəndə, a və b dəyişəninin dəyərlərinin toplamını əldə et.
+
+             Homework 2:
+             İki ölkə adını ayrı-ayrı string tipində dəyişənlərdə saxla. Sonra, bu iki string tipində dəyişənin dəyərlərini
+             üçüncü bir dəyişəndə birləşdir. Ancaq aralarında boşluq olsun.
+
+             Homework 3:
+             Char və Byte data tiplərini araşdır. Fərqli nümunələr hazırla.
+
+             Vacib not!!!!
+             Gündə ən az, 1 saat kod yazmaqla məşğul ol. Çox vacibdir!
+             */
+        }
+    }
+}
