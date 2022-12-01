e5b9075603a1c5ba2082e2963a80ba253b51cbd2
Omer Mustafa <omar201020222022@gmail.com>
2022-12-01 22:43:35 +0400

    ayin 27 olan tapsiriq hazirdi

--- Noyabr/24/OmersFirstProject/OmersFirstProject/Program.cs
+++ Noyabr/24/OmersFirstProject/OmersFirstProject/Program.cs
@@ -10,8 +10,19 @@ namespace OmersFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Salam xos gelmisiniz!");
-            Console.WriteLine("Adinizi daxil edin:");
+            //Console.WriteLine("Salam xos gelmisiniz!");
+            //Console.WriteLine("Adinizi daxil edin:");
+            var red = "qirmizi ";
+            var car = "avtomobil ";
+            var expensive = "bahali ";
+            var phone = "telefon";
+
+            Console.Write(red);
+            Console.Write(car);
+            Console.WriteLine();
+            Console.Write(expensive);
+            Console.Write(phone);
+
             Console.ReadLine();
         }
     }
---
+++ Noyabr/27/Homework/Homework/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salam xos gelmisiniz!");
+
+            Console.WriteLine("Necesiniz?");
+            var answer = Console.ReadLine();
+
+            Console.WriteLine("Adiniz nedir?");
+            var nameOFUser = Console.ReadLine();
+
+            Console.WriteLine("Hansi isde isdiyirsiniz?");
+            var job = Console.ReadLine();
+
+            Console.WriteLine("Nece hasiniz var?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Siz bos vaxtinizda niynirsiz?");
+            var hobbies = Console.ReadLine();
+
+            Console.WriteLine("Siz pensiya alirsinizmi?");
+            var pension = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
